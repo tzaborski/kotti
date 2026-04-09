fd10fedf28891c1640f2f2f598cf3fc3c58f59b0
tomasz-zaborski <tom.zaborsky@gmail.com>
2026-04-09 14:34:47 +0200

    feat(KtNotificationCentre): add link on overview page

--- packages/documentation/pages/(main)/usage/layouts/introduction/+Page.vue
+++ packages/documentation/pages/(main)/usage/layouts/introduction/+Page.vue
@@ -1,38 +1,46 @@
 <template>
 	<!-- prettier-ignore -->
 	<MarkdownBlock>
-# Introduction to Layouts
+		# Introduction to Layouts
 
-## Layout Components
+		## Layout Components
 
-![layout overiew](./layout_overview.png)
+		![layout overiew](./layout_overview.png)
 
-### [Navbar](./navbar)
+		### [Navbar](./navbar)
 
-The Navbar is the top-level navigation for the user. The width is fixed: `280px` or `14rem`. The background is `Primary-600 (#2659AB)`.
+		The Navbar is the top-level navigation for the user. The width is fixed: `280px` or `14rem`. The background is
+		`Primary-600 (#2659AB)`.
 
-### [Actionbar](./actionbar)
+		### [Notification Centre](./notification-centre)
 
-The actionbar is always located between the Navbar and Workspace, which can be omitted if necessary.
+		The Notification Centre is a component that allows users to view and manage their notifications. The
+		Notification Centre is a slide-out component that appears as an overlay on top of the main content when opened.
 
-The actionbar is considered to be an attachment to the Workspace.
-It represents addtional and important information for the Workspace.
-The width of Actionbar is also fixed: `320px` or `16rem`. The background color is `#F8F8F8`.
+		### [Actionbar](./actionbar)
 
-The elements suggested on the Actionbar are: <b>primary action button</b>,
-Secondary action buttton, information which doesn't require complex interaction,
-and `sub-menu` for the Workspace.
+		The actionbar is always located between the Navbar and Workspace, which can be omitted if necessary.
 
-### Workspace
+		The actionbar is considered to be an attachment to the Workspace.
+		It represents addtional and important information for the Workspace.
+		The width of Actionbar is also fixed: `320px` or `16rem`. The background color is `#F8F8F8`.
 
-The workspace is designed as a space for related tasks, most interactions
-should be inside this area. The width of the Workspace is responsive: `100%` of the screen width with margin `24px` to the right.
-The background is `#FFFFFF`, with dropshadow: `1px 0 1px #DBDBDB`;
+		The elements suggested on the Actionbar are: <b>primary action button</b>,
+		Secondary action buttton, information which doesn't require complex interaction,
+		and `sub-menu` for the Workspace.
 
-### Responsive Classes
+		### Workspace
 
-Kotti uses the same responsive classes as Spectre. You can [find them here](https://picturepan2.github.io/spectre/layout/responsive.html#responsive-visibility).
-</MarkdownBlock>
+		The workspace is designed as a space for related tasks, most interactions
+		should be inside this area. The width of the Workspace is responsive: `100%` of the screen width with margin
+		`24px` to the right.
+		The background is `#FFFFFF`, with dropshadow: `1px 0 1px #DBDBDB`;
+
+		### Responsive Classes
+
+		Kotti uses the same responsive classes as Spectre. You can [find them
+		here](https://picturepan2.github.io/spectre/layout/responsive.html#responsive-visibility).
+	</MarkdownBlock>
 </template>
 
 <script lang="ts">
